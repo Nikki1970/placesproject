d6baf7d5d70b8a72a80e14f1dee2be0c529120cf
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-09 18:57:34 +0530

    create index page

--- placesapp/models.py
+++ placesapp/models.py
@@ -3,4 +3,13 @@ from django.contrib.gis.db.models import PointField
 # Create your models here.
 
 class Place(models.Model):
-    location = PointField()
+    title = models.CharField(max_length=20, help_text = 'Enter the place name',null=True)
+    location = PointField(null=True)
+    description = models.TextField(max_length=2000,null=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=10,null=True)
+    city = models.CharField(max_length=15, null=True)
+    type_of_place = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.title
--- placesapp/views.py
+++ placesapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from placesapp.models import Place
 
 # Create your views here.
+def index(request):
+    num = Place.objects.all().count()
+    context = {'num': num}
+    return render(request, 'index.html', context=context)
+
