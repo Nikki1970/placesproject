55cc8196483e23ca17777b4f32adbb68e73d2e7f
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-09 19:14:57 +0530

    Create places in list view

--- placesapp/views.py
+++ placesapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from placesapp.models import Place
-
+from django.views import generic
 # Create your views here.
 def index(request):
     num = Place.objects.all().count()
     context = {'num': num}
     return render(request, 'index.html', context=context)
 
+class PlacesListView(generic.ListView):
+    model = Place
