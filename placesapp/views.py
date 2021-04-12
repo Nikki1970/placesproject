77aa46d498cfd1c261dac668c5786336f796048a
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-12 12:44:08 +0530

    fix bug in add a place

--- placesapp/views.py
+++ placesapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from placesapp.models import Place
 from django.views import generic
+from .forms import PlaceForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     num = Place.objects.all().count()
@@ -20,3 +22,14 @@ def all_cities(request):
 def city_places(request,inputcity):
     city = Place.objects.filter(city=inputcity)
     return render(request,'placesapp/city_places.html',context={"cities":city})
+
+def place_new(request):
+    if request.method == "POST":
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            place = form.save(commit=False)
+            place.save()
+            return redirect('list-of-places')
+    else:
+        form = PlaceForm()
+    return render(request, 'placesapp/place_edit.html', {'form': form})
